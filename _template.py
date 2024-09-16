bf15e6ece9387cc3d243068a2c75d91239b2d51e
Twil3akine <twil3akine@gmail.com>
2024-09-16 10:14:39 +0900

    add way to input

---
+++ _template.py
@@ -0,0 +1,29 @@
+import sys
+
+# input string "try" such as ["t", "r", "y"]
+def W():
+    return list(sys.stdin.readline().strip())
+
+# input string(s) 'red blue green' as ("red", "blue", "green")
+def S():
+    return map(str, sys.stdin.readline().strip().split())
+
+# input string(s) 'red blue green' as ["red", "blue", "green"]
+def LS(): 
+    return list(map(str, sys.stdin.readline().strip().split()))
+
+# input interger(s) '1 2 3' as (1, 2, 3)
+def I(): 
+    return map(int, sys.stdin.readline().strip().split())
+
+# input interger(s) '1 2 3' as [1, 2, 3]
+def LI(): 
+    return list(map(int, sys.stdin.readline().strip().split()))
+
+def sovle():
+    
+    
+    return 0
+
+if __name__ == "__main__":
+    sovle()
