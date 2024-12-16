1d19828393ed4d345237ac39f72fc08d14e852c2
Twil3akine <twil3akine@gmail.com>
2024-12-16 10:31:04 +0900

    add yesno

--- ratc/src/main.rs
+++ ratc/src/main.rs
@@ -1,6 +1,26 @@
-// use std::process::exit;
+use std::process::exit;
 use proconio::input;
 
+
+/* 10^18を越えるときのみ、128bitを使うこと。 */
+
+fn no(cdt: bool) -> () {
+    if cdt == false {
+        println!("No");
+        exit(0);
+    }
+}
+
+fn yes_no(cdt: bool) -> () {
+    if cdt == true {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}
+
 fn main() {
-    
+    input!(
+
+    );
 }
