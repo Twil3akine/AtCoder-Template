f387f497550a29b9e810eee7c23c21d7ff659dcf
Twil3akine <twil3akine@gmail.com>
2025-03-23 21:55:53 +0900

    ADD: Manacher Algorithm - IN: main.rs

--- ratc/src/main.rs
+++ ratc/src/main.rs
@@ -48,13 +48,41 @@ fn bound_search<T: Ord>(vector: &[T], target: T, upper: bool, reverse: bool) -> 
     right as usize
 }
 
-fn zlgorithm(target: &[char]) -> Vec<usize> {
-    let n: usize = target.len();
+fn manacher(s: &[char]) -> Vec<usize> {
+    let n: usize = s.len();
+    let mut a: Vec<usize> = vec![0; 2*n+1];
+    let (mut i, mut j): (usize, usize) = (1, 1);
+
+    while i<= 2*n {
+        // 1. 伸ばせるだけ伸ばす
+        while (j < i) && (i+j < 2*n) && (s[(i-j)/2-1] == s[(i+j)/2]) { j += 2; }
+        a[i] = j;
+        // 空区間の場合は例外処置
+        if j == 0 {
+            i += 1;
+            j = 1;
+            continue;
+        }
+        // 2. 境目に達するまで回文配列を書き写す
+        let mut k: usize = 1;
+        while (k <= i) && (k+a[i-k] < j) {
+            a[i+k] = a[i-k];
+            k += 1;
+        }
+        // 3. 境目に達したら現在の回文区間を覚えて 1. に戻る
+        i += k;
+        j -= k;
+    }
+    a.chunks_exact(2).map(|chunk| chunk[1].clone()).collect::<Vec<usize>>()
+}
+
+fn zlgorithm(s: &[char]) -> Vec<usize> {
+    let n: usize = s.len();
     let mut z: Vec<usize> = vec![0; n];
     let (mut l, mut r): (usize, usize) = (0, 0);
     for i in range!(1, n) {
         if i <= r { z[i] = z[i-l].min(r-i+1); }
-        while (i + z[i] < n) && (target[z[i]] == target[i+z[i]]) { z[i] += 1; }
+        while (i + z[i] < n) && (s[z[i]] == s[i+z[i]]) { z[i] += 1; }
         if r < i+z[i]-1 { l = i; r = i+z[i]-1; }
     }
     z
@@ -70,6 +98,7 @@ fn main() {
     }
     printvec!(s);
     printvec!(zlgorithm(&s));
+    printvec!(manacher(&s));
 }
 
 
